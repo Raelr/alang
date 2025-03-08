baa283969566300f8ca9e8941c4183e60c0fa722
aryeh <aryehzinn@hotmail.com>
2025-03-08 20:30:59 +1000

    first commit

---
+++ test/main.c
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2020-present Caps Collective & contributors
+// Originally authored by Jonathan Moallem (@jonjondev) & Aryeh Zinn (@Raelr)
+//
+// This code is released under an unmodified zlib license.
+// For conditions of distribution and use, please see:
+//     https://opensource.org/licenses/Zlib
+//
+
+#include <utest.h>
+
+// Define utest main method
+UTEST_MAIN()
